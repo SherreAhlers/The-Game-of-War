7e342d2a5c93c071f1901fd88f9a1ca18f3b328d
SherreAhlers <sherre.ahlers@gmail.com>
2020-08-07 09:26:28 -0500

    Pseudo Code

---
+++ main.js
@@ -0,0 +1,52 @@
+/*----- constants -----*/
+deck = 32 cards
+player1
+player2
+
+/*----- app's state (variables) -----*/
+let war; // if player1 && player2 === value
+// if war, 3 cards face down && 1
+// card face up
+let roundWin; // highest card
+let gameWin; // player with full deck
+
+/*----- cached element references -----*/
+document.querySelector(‘’)
+document.querySelectorAll(‘’)
+document.getElementById(‘’)
+document.createElement(‘’)
+
+/*----- event listeners -----*/
+.addEventListener(‘click’, handleClickEvent)
+.addEventListener(‘click, init)
+
+/*----- functions -----*/
+init();
+
+function roundWin() {
+// = highest card
+};
+
+
+function war() {
+// if player1 && player2 === value
+// if war, 3 cards face down && 1 card face
+// up
+};
+
+function gameWin() {
+// player === full deck
+};
+
+function init() {
+// this will initialize the game
+};
+
+function render() {
+// this will call the DOM and make changes to
+//the browser as the game progresses and
+//when game replay clicked
+};
+
+function replay() {
+// once game has been won, button will work
---
+++ readme.md
@@ -0,0 +1,12 @@
+This is the game of War!
+
+    Game Play Instructions
+The object of this game is to win all the cards in the deck.
+
+    Two players will be dealt the full deck of cards evenly - all cards will start face down.
+    Both players will flip 1 card at the same time face up - whichever player has the highest card wins that round and takes the cards on the table.
+War happens when both players turn over the same value of card.
+    - the card values go from highest to lowest - Ace, King, Queen, Jack, 10, 9, 8, 7, 6, 5, 4, 3, 2.
+Once in War - both players will flip 3 cards face down and 1 card face up - the winner is the player with the highest card, if both cards are equal in value another war begins. The winner will take all the cards on the table.
+    The game is won when one player has aqcuired all the cards in a deck.
+    
