62c538e089240fb5618fdaa9ca864b32ca3a666a
SherreAhlers <sherre.ahlers@gmail.com>
2020-08-13 11:18:45 -0500

    Update Readme

--- main.js
+++ main.js
@@ -19,9 +19,6 @@ const cardWorth = {
 
 const player1 = [];
 const player2 = [];
-let warArray = [];
-let player1Card;
-let player2Card;
 
 /*----- app's state (variables) -----*/
 let deck = []
@@ -35,25 +32,21 @@ values.forEach(v => {
     })
 });
 
-// let war; // if (player1.card === player2.card) {
-//   return player1.draw * 3 && player2.draw *3  // how do you add face up and down cards
-// if war, 3 cards face down && 1
-// card face up
-// let roundWin; // highest card
-// let gameWin; // player with full deck
-
+let warArray = [];
+let player1Card;
+let player2Card;
+let winner;
+// let scores;
 
 /*----- cached element references -----*/
-document.querySelector('#board-deck-p1')
-document.querySelector('#board-deck-p2')
-    // document.getElementById('player-1-location')
-    // document.getElementById('player-2-location')
-    // document.createElement('')
-    // document.createElement('')
+const p1Card = document.getElementById('board-deck-p1')
+const p2Card = document.querySelector('#board-deck-p2')
+const msgEl = document.querySelector('#msg');
+const replayEl = document.getElementById('replay');
 
 /*----- event listeners -----*/
 document.querySelector('#play').addEventListener('click', renderShowCards);
-// document.querySelector('#replay').addEventListener('click', init);
+// replayEl.addEventListener('click', init);
 
 /*----- functions -----*/
 function shuffle(a) {
@@ -77,16 +70,16 @@ function dealHands() {
     console.log(player1)
     console.log(player2)
     if (deck.length <= 2) {
-        // gameWin()
+        gameWin()
     }
 }
 
 function showCards() {
     player1Card = player1[player1.length - 1]
-    let p1Card = document.getElementById('board-deck-p1')
+    const p1Card = document.getElementById('board-deck-p1')
     p1Card.className = `card ${player1Card.face}`
     player2Card = player2[player2.length - 1]
-    let p2Card = document.getElementById('board-deck-p2')
+    const p2Card = document.getElementById('board-deck-p2')
     p2Card.className = `card ${player2Card.face}`
 };
 
@@ -107,14 +100,14 @@ function renderShowCards(evt) {
     if (btn === document.querySelector('#play')) {
         dealHands()
         showCards()
-        roundWin()
+        roundWin() // if so need message/ or keep round score - which player won round
     };
 };
 
 function roundWin() {
     if (player1Card.worth > player2Card.worth) {
         console.log('player1 wins!')
-            // return player1
+            // return player1 // need to find a way to put message into DOM
         player1.unshift(player2Card)
         player2.pop()
         player1.unshift(player1.pop())
@@ -129,7 +122,7 @@ function roundWin() {
         while (warArray.length) {
             player2.unshift(warArray.pop())
         }
-        // return player2
+        // return player2 // need to find a way to put message into DOM
     } else {
         console.log('tie, run war function')
         for (let i = 0; i < 2; i++) {
@@ -139,51 +132,59 @@ function roundWin() {
         setTimeout(() => {
             showCards()
             roundWin()
-        }, 3000);
-        //player2Card = 
-        //     war();
-    }
-    if (player1.length === 0) {
-        console.log('game over player 2 wins!')
-    } else if (player2.length === 0) {
-        console.log('game over player 1 wins!')
-    }
-    // need to figure out how to push cards to player who wins round
-};
+            war()
+        }, 2000);
+        // }
+        // if (player1.length === 0) {
+        //     // gameWin()
+        //     // console.log('game over player 1 wins!') // find way to make this a DOM message
+        // } else if (player2.length === 0) {
+        //     // gameWin()
+        // }
+    };
 
+    function war() {
+        dealHands()
+        dealHands()
+        dealHands()
+        dealHands()
+        roundWin()
 
-function war() {
-    dealHands()
-    dealHands()
-    dealHands()
-    dealHands()
-    roundWin()
+    };
 
-};
-
-function gameWin() {
-    let winner;
-    if (player1.length === deck.length) {
-        winner = player1
-    } else if (player2.length === deck.length) {
-        winner = player2
-    }
-    return winner; // use this to render winner of game
-};
-
-// function renderRoundWinner() {
-
-// };
-
-// function renderWinnerScore() {
-
-// };
-
-function render() {
-    renderShuffleDeck();
-    renderShowCards();
-    // renderRoundWinner();
-    // if (winner) {
-    //     renderWinnerScore();
+    // function gameWin() {
+    //     return player1.length === 0 || player2.length === 0
     // }
-};
+
+
+    function gameWin() {
+        if (player1.length === deck.length) {
+            winner = player1
+        } else if (player2.length === deck.length) {
+            winner = player2
+        }
+        return winner;
+    };
+
+    function render() {
+        renderShuffleDeck();
+        renderShowCards();
+        // renderRoundWinner();
+        // replayEl.style.visibility = gameWin() ? 'visible' : 'hidden';
+        // player1 = deck.length
+        // if (player1.length === deck.length) {
+        //     winner = player1
+        // } else if (player2.length === deck.length) {
+        //     winner = player2
+        // }
+
+        renderMessage();
+    };
+
+    function renderMessage() {
+        if (player1.length === deck.length) {
+            msgEl.innerHTML = "Congratulations Player 1, you have won!";
+        } else if (player2.length === deck.length) {
+            msgEl.innerHTML = "Congratulations Player 2, you have won!";
+        }
+    };
--- readme.md
+++ readme.md
@@ -1,12 +1,43 @@
-This is the game of War!
+# *This is the Game of War!*
 
-    Game Play Instructions
-The object of this game is to win all the cards in the deck.
+This is a fun and exciting card game played by two players!...
+The object of the game is too win all the cards in the deck.
 
-    Two players will be dealt the full deck of cards evenly - all cards will start face down.
-    Both players will flip 1 card at the same time face up - whichever player has the highest card wins that round and takes the cards on the table.
-War happens when both players turn over the same value of card.
-    - the card values go from highest to lowest - Ace, King, Queen, Jack, 10, 9, 8, 7, 6, 5, 4, 3, 2.
-Once in War - both players will flip 3 cards face down and 1 card face up - the winner is the player with the highest card, if both cards are equal in value another war begins. The winner will take all the cards on the table.
-    The game is won when one player has aqcuired all the cards in a deck.
-    
+<img src="https://i.imgur.com/C0yn2D5.jpg" width="900">
+
+
+![Screenshot] (https://i.imgur.com/AJXFFGF.jpg")
+
+## Game Play Instructions
+
+## The object of this game is to win all the cards in the deck! 
+
+## Two players will be dealt the full deck of cards evenly and face down.
+
+## Both players will flip 1 card at the same time, whoever has the highest. 
+
+## card wins that round and takes the cards on the table.
+
+## The card values go from highest to lowest.
+
+## Ace, King, Queen, Jack, 10, 9, 8, 7, 6, 5, 4, 3, 2.
+
+## War happens when both players turn over the same value of card.
+
+## Once in War, both players will flip 3 cards face down and 1 card face up.
+
+## The winner is the player with the highest card, if both cards are equal in...
+
+## value another war begins!
+
+## The winner will take all the cards on the table.
+
+## The game is won when one player has aqcuired all the cards in a deck.
+
+### Click Here To Play: <https://sherreahlers.github.io/The-Game-of-War/>
+
+## Technologies Used:
+For this game I used JavaScript, HTML, CSS.
+
+## Pseudo Code for Game of War
+
